9cbfc8b067318b769e1066807684127fb0a20f03
Panadsada <b6118693@g.sut.ac.th>
2023-04-29 12:44:56 +0700

    upfile frontend - close #4

---
+++ frontend/src/models/IAdmin.ts
@@ -0,0 +1,7 @@
+export interface AdminInterface {
+	IDadmin?: 		number,
+	Name?: 		string,
+	Email?: 	string,
+	Password?: 	string,
+	Role?: 		string,
+}
---
+++ frontend/src/models/ISell.ts
@@ -0,0 +1,30 @@
+import {AdminInterface} from "./IAdmin";
+import {UserInterface} from "./IUser";
+import {UnitInterface} from "./IUnit";
+import {TradeInterface} from "./ITrade";
+import {DrugInterface} from "./IDrug";
+
+export interface SellInterface {
+	IDsell: 		number,
+    Quantity:   number,
+    Cost: 		number,
+	Total: 	    number,
+	Payment:  	number,
+    SellTime:   Date | null,
+
+
+    AdminID:    number,
+    Admin:      AdminInterface,
+
+    UserID:     number,
+    User:       UserInterface,
+
+    UnitID:     number,
+    Unit:       UnitInterface,
+
+    TradeID:    number,
+    Trade:      TradeInterface,
+
+    DrugID:     number,
+    Drug:       DrugInterface,
+}
